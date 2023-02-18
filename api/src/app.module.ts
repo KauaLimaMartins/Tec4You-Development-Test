9cd7b02a9d0c09f7ca0bddb91246f0f51d9f1e69
KauaLimaMartins <kaualimadev0601@gmail.com>
2023-02-17 22:41:28 -0300

    fix bug

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 
 import { CovidandoModule } from './covidando/covidando.module';
